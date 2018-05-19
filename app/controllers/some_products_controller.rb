a58669821944390bae58437881cfbd16bef8c6ca
Estiven <salazar-moncada-estiven@hotmail.com>
2018-05-19 03:12:02 -0500

    cambios en las vistas del administrador, en el index, aplication_controller y en las vistas de devise

--- app/controllers/some_products_controller.rb
+++ app/controllers/some_products_controller.rb
@@ -1,5 +1,6 @@
 class SomeProductsController < ApplicationController
   before_action :set_some_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /some_products
   # GET /some_products.json
